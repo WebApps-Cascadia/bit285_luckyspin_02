ff871c395b1da4a9e9834581560180936d33e86e
Chad <chadloswald@gmail.com>
2023-01-10 17:13:15 -0800

    I think done

--- LuckySpin/Program.cs
+++ LuckySpin/Program.cs
@@ -1,10 +1,12 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<LuckySpin.Models.TextTransform>();
 
 /* Install Services using the builder.Services methods
  */
 
 //TODO: use the AddMvc method to enable MVC for this application
 
+builder.Services.AddMvc();
 
 //TODO: DIJ Part 1: Register the TextTranform class as available for DIJ
 
@@ -16,18 +18,21 @@ var app = builder.Build();
  */
 app.UseStaticFiles();
 
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Spinner/Error");
 }
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller}/{action}/{luck}",
-    defaults: new {
+    pattern: "{controller}/{action}/{luck:range(1,9)}",
+    defaults: new
+    {
         controller = "Spinner",
         action = "Index",
         luck = 7
     });
 
 app.Run();
+
 
